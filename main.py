bd05d14b8da858078897c409e8796d385477e79f
Jack Schwarz <jackschwarz58@gmail.com>
2020-06-15 14:42:32 -0500

    Changed outgoing number to service ID

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import config
 
 client = Client(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN)
 
+message = client.messages \
+    .create(
+        body="Texting, Texting. 1, 2, 3. Is this thing on?",
+        messaging_service_sid ='MGd36eca31a7f5b39044832032b917db63',
+        to='+14054712092'
+    )
 
+print(message.sid)
