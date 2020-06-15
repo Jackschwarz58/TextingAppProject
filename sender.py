2bb96a9cec3a44ae49d8e9ba7b2d87b4b0910385
Jack Schwarz <jackschwarz58@gmail.com>
2020-06-15 15:58:42 -0500

    Added Comments and wrapped code in a function

--- sender.py
+++ sender.py
@@ -3,12 +3,16 @@ import config
 
 client = Client(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN)
 
-message = client.messages \
-    .create(
-        body="Texting, Texting. 1, 2, 3. Is this thing on?",
-        messaging_service_sid ='MGd36eca31a7f5b39044832032b917db63',
-        to='+14054712092',
-        status_callback='https://postb.in/1592252035200-6130564089398'
-    )
+## Sends a messages to the specified recipient with text defined by the message_text param
+## recipient: formatted phone number to which the message will be sent
+## `message_text`: message text, since we are on the trial version this will be prefixed with "Sent from your Twilio trial account"
+def send_message(recipient, message_text):
+    message = client.messages \
+        .create(
+            body= message_text,
+            messaging_service_sid ='MGd36eca31a7f5b39044832032b917db63',
+            to= recipient,
+            status_callback='https://postb.in/1592252035200-6130564089398' # Test callback to postbin for review, REPLACE LATER
+        )
+    print(message.sid) # Prints SID to confirm message sent
 
-print(message.sid)
