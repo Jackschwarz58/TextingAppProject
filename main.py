08db7ae8cdb6e54afa97c927b5e0f2acddbb5439
Jack Schwarz <jackschwarz58@gmail.com>
2020-06-15 15:16:35 -0500

    Testing call backs, not final

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ message = client.messages \
     .create(
         body="Texting, Texting. 1, 2, 3. Is this thing on?",
         messaging_service_sid ='MGd36eca31a7f5b39044832032b917db63',
-        to='+14054712092'
+        to='+14054712092',
+        status_callback='https://postb.in/1592252035200-6130564089398'
     )
 
 print(message.sid)
