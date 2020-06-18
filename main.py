d3d36ca1194f349375428a7b3242b0afeb2e92d8
Jack Schwarz <jackschwarz58@gmail.com>
2020-06-18 01:25:44 -0500

    Moved functions into parse file. This may change later.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+from parse import handleReponse
 # from helper import load_user_list
 # from sender import send_message
 
@@ -10,22 +11,8 @@ app = Flask(__name__)
 def incoming_sms():
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
-    print(body)
-    print(str(request.data))
-
-    # Start our TwiML response
-    resp = MessagingResponse()
-
-    # Determine the right reply for this message
-    if body.lower() == 'yes':
-        msg = resp.message("Wow! You Replied Yes")
-    elif body.lower() == 'no':
-        msg = resp.message("Shit! You Replied No")
-    else:
-        msg = resp.message(
-            'You really don\'t like following directions huh? Try again smartass')
-
-    msg.media('https://i.imgur.com/FbIRYFr.png')
+    print(request.values)
+    resp = handleReponse(body, request)
 
     return str(resp)
 
