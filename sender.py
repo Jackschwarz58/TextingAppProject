f433d612f4fd38ad77d5430ce957d437320fbeec
Jack Schwarz <jackschwarz58@gmail.com>
2020-06-17 23:23:34 -0500

    For test purposes

--- sender.py
+++ sender.py
@@ -1,17 +1,25 @@
+
 from twilio.rest import Client
 import config
 
 client = Client(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN)
 
-## Sends a messages to the specified recipient with text defined by the message_text param
-## recipient: formatted phone number to which the message will be sent
-## `message_text`: message text, since we are on the trial version this will be prefixed with "Sent from your Twilio trial account"
+# Sends a messages to the specified recipient with text defined by the message_text param
+# recipient: formatted phone number to which the message will be sent
+# `message_text`: message text, since we are on the trial version this will be prefixed with "Sent from your Twilio trial account"
+
+
 def send_message(recipient, message_text):
     message = client.messages \
         .create(
-            body= message_text,
-            messaging_service_sid = config.TWILIO_SERVICE_SID,
-            to = recipient,
-            status_callback='https://postb.in/1592278750490-7459223824553' # Test callback to postbin for review, REPLACE LATER
+            body=message_text,
+            messaging_service_sid=config.TWILIO_SERVICE_SID,
+            to=recipient,
+            status_callback='https://postb.in/1592452643765-1927637152839'
+            # Test callback to postbin for review, REPLACE LATER
         )
-    print(message.sid) # Prints SID to confirm message sent
+    print(message.sid)  # Prints SID to confirm message sent
+
+
+send_message('+14058085488',
+             'Hello! Please help me out and respond \'yes\' or \'no\'')
