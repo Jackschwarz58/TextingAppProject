c0ed18a32babe53cd54e6597a9ea944e7044183e
Jack Schwarz <jackschwarz58@gmail.com>
2020-06-23 23:25:40 -0500

    Started to test/implement NLP using TextBlob

--- parse.py
+++ parse.py
@@ -1,8 +1,9 @@
 from twilio.twiml.messaging_response import MessagingResponse
+from textblob import TextBlob
 
 
 def handleReponse(body, request):
-    print('BODY TEXT: \'' + body+'\'')
+    print('BODY TEXT: \'' + body + '\'')
     # Start our TwiML response
 
     messageText = ''
@@ -20,7 +21,9 @@ def handleReponse(body, request):
     elif 'test' in bodyContent:
         messageText += str(request.values.to_dict)
     else:
-        messageText += 'Sorry, I didn\'t catch that. What was that again? (This command may not be implemented yet)'
+        #messageText += 'Sorry, I didn\'t catch that. What was that again? (This command may not be implemented yet)'
+        sentimentText = TextBlob(bodyContent)
+        messageText += 'Positive Sentiment' if sentimentText.polarity >= 0 else 'Negative Sentiment'
 
     resp.message(messageText)
     return resp
